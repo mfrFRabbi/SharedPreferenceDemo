56e8a14c661cca15c49abe34f6690c21e29bb4c2
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-20 13:22:22 +0600

    initial commit

--- app/src/main/java/com/example/sharedpreferencedemo/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencedemo/MainActivity.java
@@ -13,8 +13,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText userName,password;
-    private TextView textView;
-    private Button saveBtn,loadBtn;
+    private TextView textView,incrementText;
+    private Button saveBtn,loadBtn,incrementBtn,decrementBtn;
+    private int increment = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView = findViewById(R.id.textViewId);
         saveBtn = findViewById(R.id.saveBtnId);
         loadBtn = findViewById(R.id.loadBtnId);
+        incrementText = findViewById(R.id.incrementTextId);
+        incrementBtn = findViewById(R.id.incrementBtnId);
+        decrementBtn = findViewById(R.id.decrementBtnId);
+
+        if(loadIncreDecreResult() != 0){
+            increment = loadIncreDecreResult();
+            incrementText.setText(String.valueOf(increment));
+        }
+
 
         saveBtn.setOnClickListener(this);
         loadBtn.setOnClickListener(this);
+
+        incrementBtn.setOnClickListener(this);
+        decrementBtn.setOnClickListener(this);
     }
 
     @Override
@@ -36,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String username = userName.getText().toString();
             String pass = password.getText().toString();
 
-            if(username.equals(null) && pass.equals(null)){
+            if(username.equals("") && pass.equals("")){
                 Toast.makeText(getBaseContext(),"Please give some input!",Toast.LENGTH_SHORT).show();
             }else {
                 SharedPreferences sharedPreferences = getSharedPreferences("UserDetails", Context.MODE_PRIVATE);
@@ -59,5 +72,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 textView.setText(username+ " " + pass);
             }
         }
+
+        if(v.getId() == R.id.incrementBtnId){
+            if(increment>=0){
+                ++increment;
+                incrementText.setText(String.valueOf(increment));
+                saveIncreDecreResult(increment);
+            }
+
+
+
+        }
+
+        if(v.getId() == R.id.decrementBtnId){
+            if(increment>=1){
+                --increment;
+                incrementText.setText(String.valueOf(increment));
+                saveIncreDecreResult(increment);
+            }
+
+        }
+    }
+
+    private void saveIncreDecreResult(int increment) {
+        SharedPreferences preferences = getSharedPreferences("InC_De",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("score",increment);
+        editor.apply();
+    }
+    private int loadIncreDecreResult() {
+        SharedPreferences preferences = getSharedPreferences("InC_De",Context.MODE_PRIVATE);
+        if(preferences.contains("score")){
+           return preferences.getInt("score",0);
+        }
+        return 0;
     }
 }
